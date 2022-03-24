784fc55eb2a498aa53f5f7b4602f28908cccc8dd
Simon <simon.simon@student.howest.be>
2022-03-24 23:48:48 +0100

    ok holy shit dit ga mss nog werken

---
+++ src/chart.js
@@ -0,0 +1,13 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded",init)
+
+function init ()
+{
+    console.log("yeet");
+
+    window.setTimeout( function() {
+        window.location.reload();
+    }, 5000);
+
+}
